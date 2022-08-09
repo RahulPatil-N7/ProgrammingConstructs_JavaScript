93e7a461d0190c07de139267cfe02850df7fce70
RahulPatil-N7 <rahul.patil.3125@gmail.com>
2022-08-10 02:04:42 +0530

    Added : Read number to Unit using switch case.

---
+++ NumberToUnitSwitch.js
@@ -0,0 +1,19 @@
+const prompts = require("prompt-sync")();
+let num = prompts("Enter a number : ")
+switch (parseInt(num)) {
+    case 1: console.log("Unit");
+        break;
+    case 10: console.log("Ten");
+        break;
+    case 100: console.log("Hundred");
+        break;
+    case 1000: console.log("Thousand");
+        break;
+    case 10000: console.log("Ten Thousand");
+        break;
+    case 100000: console.log("Hundred Thousand");
+        break;
+    case 1000000: console.log("One Million");
+        break; 
+	default: console.log("Enter a valid number.");
+}
